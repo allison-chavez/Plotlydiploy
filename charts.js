233e2cdeb12121b153f469a72f66418c252fa6a1
allison-chavez <allchavez@utexas.edu>
2021-06-20 02:10:15 -0500

    completed bubble code, test for accuracy and errors

--- charts.js
+++ charts.js
@@ -92,5 +92,42 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", trace, barLayout);
+
+    });
+}  
+  // Bar and Bubble charts
+  // Create the buildCharts function.
+  function buildCharts(sample) {
+    // Use d3.json to load and retrieve the samples.json file 
+    d3.json("samples.json").then((data) => {
+    
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot(); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      type: "bubble",
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+
+      }
+
+    }];
+    
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU IDS"}
+      showlegend: false,
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
 }
