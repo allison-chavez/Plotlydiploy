82c46971560932a856d703ac8fba40067cd4cf72
allison-chavez <allchavez@utexas.edu>
2021-06-20 11:26:59 -0500

    finishing and testing gauge code

--- charts.js
+++ charts.js
@@ -118,6 +118,32 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        axis: {range: [null, 10]},
+        bar: { color: "black"},
+        steps: [
+        { range: [0, 2], color: "red" },
+        { range: [2, 4], color: "orange" },
+        { range: [4, 6], color: "yellow" },
+        { range: [6, 8], color: "lightgreen" },
+        { range: [8, 10], color: "green" }],
+    }
+  ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      title: "Belly Button Washing Frequency" 
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("indicator", gaugeData, gaugeLayout);
+
   });
 }
+
 
